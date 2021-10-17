33e1a3259b902b2c1d4faae1ef11d09bd0d3c5bd
alu0101243498 <72921642+alu0101243498@users.noreply.github.com>
2021-10-17 11:34:27 +0100

    Add files via upload

---
+++ pr1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pr1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Esta es mi prueba");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ queen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class queen : MonoBehaviour
+{
+    private const int ID = 2;
+    private int contador = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Objeto: " + gameObject.name + " con ID: " + ID);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Iteración " + contador + " de " + gameObject.name);
+        contador++;
+    }
+}
